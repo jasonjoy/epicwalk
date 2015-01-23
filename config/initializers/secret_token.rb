ba288ce95a3b50737ca406ae085e517cb1a5a17f
Jason Joy <jasondjoy@gmail.com>
2015-01-23 05:25:02 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Epicwalk::Application.config.secret_key_base = 'a99bfacfd80596a6934f7449dca85754f0b6c88417dced96cccf3a59ec7ea855a41f46d1025c03fc2baf9adbd88b986fa43f9d22d2d7a7870009dabedd360339'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Epicwalk::Application.config.session_store :cookie_store, key: '_epicwalk_session'
