41072a809c8dfed132e5d8faa38aefa65ca36b22
Jason Joy <jasondjoy@gmail.com>
2015-02-10 21:58:13 +0000

    More oauth tweaking

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,26 @@ class User < ActiveRecord::Base
 
   has_many :journeys
 
+  def linked?
+    oauth_token.present? && oauth_secret.present?
+  end
+
+  def fitbit_data
+    raise "Account is not linked with a Fitbit account" unless linked?
+    @client ||= Fitgem::Client.new(
+                :consumer_key => ENV["FITBIT_CONSUMER_KEY"],
+                :consumer_secret => ENV["FITBIT_CONSUMER_SECRET"],
+                :token => oauth_token,
+                :secret => oauth_secret,
+                :user_id => uid
+              )
+  end
+
+  def has_fitbit_data?
+    !@client.nil?
+  end
+
+
   def calculate_distance
 
 	config = {:consumer_key => ENV["FITBIT_CONSUMER_KEY"], :consumer_secret => ENV["FITBIT_CONSUMER_SECRET"]}
