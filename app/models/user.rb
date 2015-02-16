9dc922e955b1ecce6ed042d8515c68d877a995bb
Jason Joy <jasondjoy@gmail.com>
2015-02-16 21:06:42 +0000

    User.calculate_distance working

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,27 +26,7 @@ class User < ActiveRecord::Base
   end
 
 
-  def calculate_distance
-
-	config = {:consumer_key => ENV["FITBIT_CONSUMER_KEY"], :consumer_secret => ENV["FITBIT_CONSUMER_SECRET"]}
-	client = Fitgem::Client.new(config)
-
-	begin
-		access_token = client.authorize(token, secret, { :oauth_verifier => verifier })
-	rescue Exception => e
-	    puts "Error: Could not authorize Fitgem::Client with supplied oauth verifier"
-	    exit
-	end
-
-	puts 'Verifier is: '+verifier
-	puts "Token is:    "+access_token.token
-	puts "Secret is:   "+access_token.secret
-
-	user_id = client.user_info['user']['encodedId']
-	puts "Current User is: "+user_id
-
-	config[:oauth].merge!(:token => access_token.token, :secret => access_token.secret, :user_id => user_id)
-
+  def calculate_distance(start_date, end_date)
 
 	# Write the whole oauth token set back to the config file
 	# File.open(".fitgem.yml", "w") {|f| f.write(config.to_yaml) }
@@ -54,18 +34,16 @@ class User < ActiveRecord::Base
   	# ============================================================
 	# Add Fitgem API calls on the client object below this line
   
-	p = client.activity_on_date_range 'distance','2014-12-01','2014-12-31'
+	p = fitbit_data.activity_on_date_range 'distance',start_date.strftime('%Y-%m-%d'),end_date.strftime('%Y-%m-%d')
 	dist = p["activities-distance"]
 
 	collection = dist.collect do |day|
 	  day["value"].to_f
 	end
-
-	# Puts each day
-	puts collection.inspect
-	puts collection.sum
+	value = {
+		:collection => collection, :sum => collection.sum
+	}
 
   end
-
 
 end
